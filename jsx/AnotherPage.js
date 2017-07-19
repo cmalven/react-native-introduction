d92a3174dbb90704de5f1de9878a64fff27e44c8
Chris Malven <cmalven@chrismalven.com>
2017-07-19 13:33:52 -0500

    Adds example of fetching data from an API.

--- jsx/AnotherPage.js
+++ jsx/AnotherPage.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default class AnotherPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Loading Name',
+      location: 'Loading Location',
+      imageSrc: 'https://placeimg.com/200/200/any/grayscale'
+    };
+  }
+
+  async _fetchUser() {
+    let url = 'https://api.github.com/users/cmalven';
+    let data = await fetch(url);
+    return data.json();
+  }
+
+  componentDidMount() {
+    this._fetchUser().then((data) => {
+      this.setState((prevState, props) => {
+        return {
+          name: data.name,
+          location: data.location,
+          imageSrc: data.avatar_url
+        };
+      });
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Current Page: { this.props.route.title }</Text>
+        <Image
+          style={{ width: 200, height: 200 }}
+          source={{ uri: this.state.imageSrc }}
+        />
+        <Text style={styles.name}>{ this.state.name }</Text>
+        <Text style={styles.location}>{ this.state.location }</Text>
       </View>
     );
   }
@@ -18,4 +50,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  name: {
+    fontSize: 32,
+    marginTop: 50
+  },
+
+  location: {
+    marginTop: 15
+  }
 });
